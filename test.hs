1a02b646e5d5864815415452fb0c56f3f9eb8f7d
David Moody <david@davidxmoody.com>
2016-05-10 18:54:17 +0100

    Add hanoi solution

--- test.hs
+++ test.hs
@@ -1,3 +1,4 @@
+-- Quick answer to question one (just based off the algorithm, not the steps)
 doubleSecond :: [Int] -> [Int]
 doubleSecond [] = []
 doubleSecond (x:(y:ys)) = (x * 2) : (y : doubleSecond ys)
@@ -12,3 +13,11 @@ getTotal xs = sum(map (sumDigits) (doubleSecond xs))
 
 isValid :: [Int] -> Bool
 isValid xs = (getTotal xs) `mod` 10 == 0
+
+-- Question two
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 a b c = [(a,b)]
+hanoi 2 a b c = [(a,c),(a,b),(c,b)]
+hanoi n a b c = concat [(hanoi (n-1) a c b), [(a, b)], (hanoi (n-1) c b a)]
